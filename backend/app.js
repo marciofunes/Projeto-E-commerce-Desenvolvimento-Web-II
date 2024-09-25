b9968b15e7e8ab13127a74e2e7ea46fcc4f96b10
Márcio Funes <marciofunes@gmail.com>
2024-09-25 15:13:57 -0300

    Código Final - Aula 3

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import productRoutes from "./routes/products.js";
 import { connectDatabase } from "./config/dbConnect.js";
+import errorMiddleware from "./middleware/erros.js";
 
 const app = express();
 dotenv.config({ path: "backend/config/config.env"});
@@ -15,6 +16,18 @@ app.use(express.json());
 //Importanto todas as rotas
 app.use("/api/v1", productRoutes);
 
-app.listen(process.env.PORT, () => {
+//Utilizando o middleware de tratamento de erros
+app.use(errorMiddleware);
+
+const server = app.listen(process.env.PORT, () => {
     console.log('Servidor iniciado na PORTA: 3000');
+});
+
+//Handle Unhandled Promise Rejections
+process.on('unhandledRejection', (err) => {
+    console.log(`ERRO: ${err}`);
+    console.log("Desligando servidor devido Rejeições de Promessas Não Tratadas");
+    server.close(() => {
+        process.exit(1);
+    });
 });
--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,4 +1,5 @@
 import Product from '../models/product.js';
+import ErrorHandler from '../utils/errorHandler.js';
 
 // Pesquisar produtos => /api/v1/products
 export const getProducts = async (req, res) => {
@@ -16,5 +17,48 @@ export const newProduct = async (req, res) => {
 
     res.status(200).json({
         product,
+    });
+};
+
+// Consultar produto por ID => /api/v1/products/:id
+export const getProductbyID = async (req, res, next) => {
+    const product = await Product.findById(req?.params?.id);
+
+    if(!product){
+        return next(new ErrorHandler("Produto não encontrado", 404));
+    }
+
+    res.status(200).json({
+        product,
+    });
+};
+
+// Atualizar produto por ID => /api/v1/products/:id
+export const updateProductbyID = async (req, res) => {
+    let product = await Product.findById(req?.params?.id);
+
+    if(!product){
+        return next(new ErrorHandler("Produto não encontrado", 404));
+    }
+
+    product = await Product.findByIdAndUpdate(req?.params?.id, req.body, { new: true });
+
+    res.status(200).json({
+        product,
+    });
+};
+
+// Deletar produto por ID => /api/v1/products/:id
+export const deleteProductbyID = async (req, res) => {
+    const product = await Product.findById(req?.params?.id);
+
+    if(!product){
+        return next(new ErrorHandler("Produto não encontrado", 404));
+    }
+
+    await product.deleteOne();
+
+    res.status(200).json({
+        message: "Produto Deletado",
     });
 };
